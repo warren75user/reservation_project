4ad558727d093d08833278d65802a227167d0e33
warren huang <warrenhuang75@gmail.com>
2024-11-21 16:48:17 -0600

    UI first version

---
+++ RestaurantReservationSystem/script.js
@@ -0,0 +1,30 @@
+document.getElementById('customerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Customer added');
+  });
+  
+  document.getElementById('reservationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Reservation submitted');
+  });
+  
+  document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Order submitted');
+  });
+  
+  document.getElementById('menuItemForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Menu item added');
+  });
+  
+  document.getElementById('staffForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Staff added');
+  });
+  
+  document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Feedback submitted');
+  });
+  
